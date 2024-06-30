68713f3560d7710a7f2bac9eea116072fde862cb
khushboo <singhkhushboo75261@gmail.com>
2024-06-30 12:57:02 +0530

    buttons completed

--- app3/lib/main.dart
+++ app3/lib/main.dart
@@ -15,15 +15,39 @@ class MyApp extends StatelessWidget {
           title:const Text("My first app")
         ),
         body: Center(
-          child: Container(
-            alignment: Alignment.center,
-           color:Colors.blue,
-           height:100,
-           width:100,
-           child:const Text("hello world"),
-          
+
+          child: Column(
+            children: [
+              Container(
+                padding:const EdgeInsets.all(20),
+                alignment: Alignment.center,
+               color:Colors.blue,
+               height:100,
+               width:100,
+               child:const Text("hello world",
+               style:TextStyle(color:Colors.white,fontSize:20,fontWeight:FontWeight.bold),)
+              
+              ),
+       TextButton(onPressed: () {  },
+       child: const Text("TextButton")),
+       const SizedBox(height:20),
+       ElevatedButton(onPressed: () {  },
+       child: const Text("ElevatedButton")),
+        const SizedBox(height:20),
+       OutlinedButton(onPressed: () {  },
+       child: const Text("OutlinedButton"))
+            ],
           ),
         ), 
+        //types of button: buttons are the graphical control element that provides 
+        //a user to trigger an event such as taking actions,making choices,searching things,and many more
+        //1. ElevatedButton(raised button)
+        //2. TextButton(flat button)
+        //3. OutlinedButton
+        //4. IconButton
+        //5. FloatingActionButton
+
+
         )
     );
   }
